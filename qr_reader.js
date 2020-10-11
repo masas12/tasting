ebdcce8c6e55625984be056f4b95c347b977ef84
masas12 <70789315+masas12@users.noreply.github.com>
2020-10-11 18:08:42 +0900

    Create qr_reader.js

---
+++ qr_reader.js
@@ -0,0 +1,11 @@
+// On Streaming
+const startStreamingVideo = () => {
+  const video = document.querySelector( "#video" );
+  if( navigator.mediaDevices.getUserMedia ){
+    navigator.mediaDevices.getUserMedia( { video: true } )
+    .then( ( stream ) => {
+        video.srcObject = stream;
+    } );
+  }
+}
+startStreamingVideo();
