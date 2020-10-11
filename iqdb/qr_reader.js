ba857582cd0bbb08ddb593fd4e04d10bdb4fef3c
masas12 <70789315+masas12@users.noreply.github.com>
2020-10-11 18:32:14 +0900

    Update qr_reader.js

--- iqdb/qr_reader.js
+++ iqdb/qr_reader.js
@@ -1,8 +1,7 @@
 let p = navigator.mediaDevices.getUserMedia({ audio: false, video: true });
 
 p.then(function(stream) {
-   var video = document.querySelector('video');
-   video.src = window.URL.createObjectURL(mediaStream);
+   document.querySelector("video").srcObject = mediaStream;
    video.onloadedmetadata = function(e) {
       // Do something with the video here.
    };
