bff0441255b48db93c78e90c07add83a09349e1e
masas12 <70789315+masas12@users.noreply.github.com>
2020-10-11 19:39:55 +0900

    Update app.js

--- iqdb/app.js
+++ iqdb/app.js
@@ -2,13 +2,13 @@ if (!navigator.mediaDevices) {
     document.querySelector('#js-unsupported').classList.add('is-show')
 }
 
-const camera  = document.querySelector('#js-video')
+const video  = document.querySelector('#js-video')
 const canvas = document.querySelector('#js-canvas')
 const ctx    = canvas.getContext('2d')
 
 const checkImage = () => {
     // 取得している動画をCanvasに描画
-    ctx.drawImage(camera, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
 
     // Canvasからデータを取得
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
@@ -47,9 +47,9 @@ navigator.mediaDevices
         }
     })
     .then(function(stream) {
-        camera.srcObject = stream
-        camera.onloadedmetadata = function(e) {
-            camera.play()
+        video.srcObject = stream
+        video.onloadedmetadata = function(e) {
+            video.play()
             checkImage()
         }
     })
