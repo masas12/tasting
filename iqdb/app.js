c547814c41e2fb99b159ba4d16eda60f2490e6a5
masas12 <70789315+masas12@users.noreply.github.com>
2020-10-11 19:07:44 +0900

    Update app.js

--- iqdb/app.js
+++ iqdb/app.js
@@ -1,20 +1,21 @@
-const video  = document.querySelector('#js-video')
+const canvas = document.querySelector('#js-canvas')
+const ctx = canvas.getContext('2d')
 
-navigator.mediaDevices
-    .getUserMedia({
-        audio: false,
-        video: {
-            facingMode: {
-                exact: 'environment'
-            }
-        }
-    })
-    .then(function(stream) {
-        video.srcObject = stream
-        video.onloadedmetadata = function(e) {
-            video.play()
-        }
-    })
-    .catch(function(err) {
-        alert('Error!!')
-    })
+const checkImage = () => {
+    // 取得している動画をCanvasに描画
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    // Canvasからデータを取得
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+    // jsQRに渡す
+    const code = jsQR(imageData.data, canvas.width, canvas.height)
+
+    // QRコードの読み取りに成功したらモーダル開く
+    // 失敗したら再度実行
+    if (code) {
+        openModal(code.data)
+    } else {
+        setTimeout(() => { checkImage() }, 200)
+    }
+}
