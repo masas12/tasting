da49ea15238be2df6e8f867b0904c969ac7cc62a
masas12 <70789315+masas12@users.noreply.github.com>
2020-10-11 21:14:36 +0900

    Delete qr_reader.js

--- iqdb/qr_reader.js
+++
@@ -1,5 +0,0 @@
-// imageData is data written in a canvas
-const code = jsQR( imageData.data, imageData.width, imageData.height, {
-  inversionAttempts: "dontInvert",
-} );
-console.log( code.data );
