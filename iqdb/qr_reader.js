7480325e549a267a98b69182d29690d6a98d6a18
masas12 <70789315+masas12@users.noreply.github.com>
2020-10-22 17:50:08 +0900

    Update qr_reader.js

--- iqdb/qr_reader.js
+++ iqdb/qr_reader.js
@@ -8,13 +8,13 @@ const ctx    = canvas.getContext('2d')
 
 const checkImage = () => {
     // 取得している動画をCanvasに描画
-    ctx.drawImage(video, 0, 0, 200, 400)
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
 
     // Canvasからデータを取得
-    const imageData = ctx.getImageData(0, 0, 200, 400)
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
     // jsQRに渡す
-    const code = jsQR(imageData.data, 200, 400)
+    const code = jsQR(imageData.data, canvas.width, canvas.height)
 
     // QRコードの読み取りに成功したらモーダル開く
     // 失敗したら再度実行
